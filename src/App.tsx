c0f0e9af03cfdcb89a3e2a15c1e8609a7bf5a069
devjean <jeancarloleall@gmail.com>
2023-04-12 20:27:56 -0300

    project-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+type TitleProps = {
+  children: React.ReactNode;
+};
+
+const Title = ({ children }: TitleProps) => {
+  return <h1>{children}</h1>;
+};
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>
+        <span>
+          Hello <b>World</b>
+        </span>
+      </Title>
     </div>
   );
 }
